fc77bd01731449acd784e19aa39d541a38cfb287
rentanjutsu <74683663+rentanjutsu@users.noreply.github.com>
2021-02-15 18:40:25 -0500

    Updated

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        num = x
+        sum = 0
+        sign = 1
+        if num < 0:
+            sign = -1
+            num = abs(num)
+        while num > 0:
+            rem = num % 10
+            sum = sum * 10 + rem
+            num = num // 10
+        if not -2147483648 < sum < 2147483648:
+            return 0
+        return sign * sum
